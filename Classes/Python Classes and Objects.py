7ac3c9d44d43132ccd61af074386eff733d9c8ef
Akniyet <aknietnroe@gmail.com>
2026-02-15 01:09:37 +0500

    Create Python Classes and Objects.py

---
+++ Classes/Python Classes and Objects.py
@@ -0,0 +1,25 @@
+ex1
+class MyClass:
+  x = 5
+
+print(MyClass)
+
+#Answer:​
+#<class '__main__.MyClass'>
+
+ex2
+class MyClass:
+  x = 5
+
+p1 = MyClass()
+print(p1.x)
+
+#Answer:
+#5
+
+ex3
+class Person:
+  pass
+
+# having an empty class definition like this, would raise an error without the pass statement
+
